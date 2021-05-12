1cf833b47ae1219e41b224f8609e00e846110663
James Walker <james.chunho+github@gmail.com>
2021-05-12 17:51:57 +0100

    add random users to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(email: "a@a", password: "aaaaaa")
+User.create(email: "b@b", password: "bbbbbb")
+User.create(email: "c@c", password: "cccccc")
+User.create(email: "d@d", password: "dddddd")
+User.create(email: "e@e", password: "eeeeee")
