7e6e89ef2586369b4a3c214fba19e669e03c8086
James Walker <james.chunho+github@gmail.com>
2021-05-16 20:55:46 +0100

    add friend helper methods in user

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,22 @@ class User < ApplicationRecord
   # for example, User.create -> class name is "User"
   has_many :inverse_friendships, class_name: "Friendship", foreign_key: :friend_id
   # has_many :inverse_friends, through: :inverse_friendships, source: :user
+
+  def friends
+    forward_friend_ids = friendships.select("friend_id as user_id").where(accepted: true)
+    backwards_friend_ids = inverse_friendships.select(:user_id).where(accepted: true)
+    User.where(id: forward_friend_ids).or(User.where(id: backwards_friend_ids))
+  end
+
+  def received_friendships
+    # backwards_friend_ids = inverse_friendships.select(:user_id).where(accepted: false)
+    # User.where(id: backwards_friend_ids)
+    inverse_friendships.where(accepted: false)
+  end
+
+  def sent_friendships
+    # forward_friend_ids = friendships.select("friend_id as user_id").where(accepted: false)
+    # User.where(id: forward_friend_ids)
+    friendships.where(accepted: false)
+  end
 end
