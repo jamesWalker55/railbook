4c622540dab30f3d0e1e10dbfe4bfae68b2021e5
James Walker <james.chunho+github@gmail.com>
2021-05-19 16:24:42 +0100

    Generate Post scaffold

---
+++ db/migrate/20210519152429_create_posts.rb
@@ -0,0 +1,10 @@
+class CreatePosts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :posts do |t|
+      t.reference :author
+      t.text :content
+
+      t.timestamps
+    end
+  end
+end
