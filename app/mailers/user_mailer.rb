864e8fd413fb0a153bf6d9e14d752bcb4f762d24
James Walker <james.chunho+github@gmail.com>
2021-05-23 19:32:13 +0100

    copied mailer code from rails guide

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,8 @@
 class UserMailer < ApplicationMailer
+  default from: 'notifications@railbook.com'
+
+  def welcome_email
+    @user = params[:user]
+    mail(to: @user.email, subject: 'Welcome to Railbook!')
+  end
 end
