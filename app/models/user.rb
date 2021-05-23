de16a782ddce476b1818958e02c886ff2c28a6f8
James Walker <james.chunho+github@gmail.com>
2021-05-23 16:33:17 +0100

    cleanup user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,38 +12,10 @@ class User < ApplicationRecord
   has_many :like_relations, dependent: :delete_all
   has_many :comments, dependent: :delete_all
   has_one_attached :image, dependent: :delete_all
-  # has_many :friends, through: :friendships
 
   # class_name is literally the name of the class
   # for example, User.create -> class name is "User"
   has_many :inverse_friendships, class_name: "Friendship", foreign_key: :friend_id
-  # has_many :inverse_friends, through: :inverse_friendships, source: :user
-
-  FRIENDSHIP_QUERY_HASH = {
-    :friends => -> (u1, u2) { u1.friendships.where(friend_id: u2, accepted: true).or(u1.inverse_friendships.where(user_id: u2, accepted: true)).limit(1) },
-    :sent_friendship => -> (u1, u2) { u1.sent_friendships.where(friend_id: u2).limit(1) },
-    :received_friendship => -> (u1, u2) { u1.received_friendships.where(user_id: u2).limit(1) },
-  }
-
-  def self.a
-    User.find_by(email: "a@a")
-  end
-
-  def self.b
-    User.find_by(email: "b@b")
-  end
-
-  def self.c
-    User.find_by(email: "c@c")
-  end
-
-  def self.d
-    User.find_by(email: "d@d")
-  end
-
-  def self.e
-    User.find_by(email: "e@e")
-  end
 
   def friends(include_self: false)
     forward_friend_ids = friendships.select("friend_id as user_id").where(accepted: true)
@@ -74,10 +46,6 @@ class User < ApplicationRecord
 
   def has_friendship
     friends.or(sent_friendships(return_users: true)).or(received_friendships(return_users: true))
-  end
-
-  def query_friendship(user, status)
-    FRIENDSHIP_QUERY_HASH[status].call(self, user)
   end
 
   def get_friendship(user)
